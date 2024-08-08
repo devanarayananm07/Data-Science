1c3b5bec4e440603a2a319e3e332e932a48444d3
DEVANARAYANAN M <154665307+devanarayananm07@users.noreply.github.com>
2024-08-07 21:52:40 -0700

    Update Gcd.py

--- Gcd.py
+++ Gcd.py
@@ -3,3 +3,14 @@ x=int(input("Enter 1st number:"))
 y=int(input("Enter 2nd number:"))
 print("The gcd of", x , "and" , y ,"is : ", end=" ")
 print(math.gcd(x, y))
+
+
+    OUTPUT
+  =========
+
+C:\Users\mlm\PycharmProjects\pythonProject\venv\Scripts\python.exe C:\Users\mlm\PycharmProjects\pythonProject\gcd.py 
+Enter 1st number:60
+Enter 2nd number:48
+The gcd of 60 and 48 is :  12
+
+Process finished with exit code 0
