3c0955c87871257d41296c628b608695ae5ecbf4
DEVANARAYANAN M <154665307+devanarayananm07@users.noreply.github.com>
2024-08-07 21:50:18 -0700

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -10,3 +10,14 @@ for i in range(n):
             arr[i]=arr[j]
             arr[j]=temp
 print("Sorted array:",arr)
+
+
+   OUTPUT
+===========
+
+C:\Users\mlm\PycharmProjects\pythonProject\venv\Scripts\python.exe C:\Users\mlm\PycharmProjects\pythonProject\gcd.py 
+Enter 1st number:60
+Enter 2nd number:48
+The gcd of 60 and 48 is :  12
+
+Process finished with exit code 0
