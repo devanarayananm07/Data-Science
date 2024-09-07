c506e45d0b6baf5b7c74982e6f276342b5bbbf71
DEVANARAYANAN M <154665307+devanarayananm07@users.noreply.github.com>
2024-09-07 02:41:25 -0700

    Update and rename 9.Histigram to 9.Histigram.py

--- 9.Histigram.py
+++ 9.Histigram.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-ages=[5,12,18,22,25,2726,30,34,38,40,42,46,45,47,49,51,56,53,67,68,64,62,70,72,74,76,77,80]
+ages=[5,12,18,22,25,27,26,30,34,38,40,42,46,45,47,49,51,56,53,67,68,64,62,70,72,74,76,77,80]
 plt.hist(ages, bins=5, color='g' , edgecolor='black')
 plt.title('Age Distribution')
 plt.xlabel('Age Ranges')
