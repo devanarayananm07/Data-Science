192eec329b9b533419458669f74f3d27bed7198f
DEVANARAYANAN M <154665307+devanarayananm07@users.noreply.github.com>
2024-10-22 21:16:48 -0700

    Create 15.Clustering.py

---
+++ 15.Clustering.py
@@ -0,0 +1,35 @@
+import pandas as pd
+customers = pd.read_csv('customer_data.csv')
+print(customers.head())
+#Visualize data Annual Income vs spending Score
+import matplotlib.pyplot as plt
+points = customers.iloc[:, 3:5].values
+x = points[:, 0]
+y = points[:, 1]
+plt.scatter(x, y, s=50, alpha=0.7)
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score')
+plt.show()
+#Using k means clustering create 6 clusters of customers based on their spending pattern.Visualize the same in a scatter plot with each cluster in a different color scheme.
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=6, random_state=0)
+kmeans.fit(points)
+predicted_cluster_indexes = kmeans.predict(points)
+plt.scatter(x, y, c=predicted_cluster_indexes, s=50, alpha=0.7, cmap='viridis')
+centers = kmeans.cluster_centers_
+plt.scatter(centers[:, 0], centers[:, 1], c='red', s=100)
+plt.show()
+#print cluster index for each data point after applying K Means
+print("\nPredicted Cluster Indexes:\n",predicted_cluster_indexes)
+
+
+    OUTPUT
+   =======
+
+C:\Users\MLM\PycharmProjects\pythonProject\DEVAN\Scripts\python.exe C:\Users\MLM\PycharmProjects\pythonProject\DEVAN\kmean.py 
+   CustomerID  Gender  Age  Annual Income (k$)  Spending Score (1-100)
+0           1    Male   19                  15                      39
+1           2    Male   21                  15                      81
+2           3  Female   20                  16                       6
+3           4  Female   23                  16                      77
+4           5  Female   31                  17                      40
